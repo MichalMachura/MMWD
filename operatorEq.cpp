3e05a374fa6e37abc895ef859c97ed1d3b3e5a2a
MichalMachura <44579524+MichalMachura@users.noreply.github.com>
2018-11-19 22:49:17 +0100

    Update operatorEq.cpp
    komentarze

--- operatorEq.cpp
+++ operatorEq.cpp
@@ -1,14 +1,18 @@
 #include "DayActions.hpp"
 
-DayAction& DayActions::operator=(const DayAction& second)
+DayActions& DayActions::operator=(const DayAction& second)
 {
-    DayActions first;
+    DayActions first;   //zamiast first bardziej *this
+                        //albo jak już to: DayActions& first = *this;
+                        //i może zostać reszta wtedy bo mamy referencje
     first.modified = second.modified;
     first.updated_factors = second.updated_factors;
-    first.collection = second.collection;
+    first.collection = second.collection;       //i to jest ta istotna rzecz dla której  był przeciążony operator=
+                                                //trzeba w petli tworzyć nowe obiekty typu action za pomocą metody virtual Action* Action::clone()   
     first.goal_function = second.goal_function;
     first.class_types = second.class_types;
     first.start_ = second.start_;
     first.end_ = second.end_;
     first.goal_function_value = second.goal_function_value;
+    //przytało by się coś zwrócić np. return *this;
 }
